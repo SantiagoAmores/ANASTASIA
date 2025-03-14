afb914ee6240d58c9008f889b727b3b5f314d9da
SantiagoAmores <santi251998@gmail.com>
2025-03-14 11:49:01 +0100

    Input Manager
    Instalacion del input manager

--- Assets/Scripts/MovimientoJugador.cs
+++ Assets/Scripts/MovimientoJugador.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class MovimientoJugador : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Para el movimiento del personaje usaremos el Input Manager de Unity que nos va a permitir exportarlo a diferentes dispositivos sin modificar el script,
+    // personalizar los controles y mantener odenado el código
+
     void Start()
     {
         
