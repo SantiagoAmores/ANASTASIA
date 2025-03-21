4efd360b9d3349fb48b0149c8612b686ef889743
Heidism15 <heidism1508@gmail.com>
2025-03-21 10:25:27 +0100

    Enemigos scripts
    Ahora los enemigos spawnean aleatoriamente y se destruyen si les disparas

--- Assets/Scripts/Scripts Enemigos/Enemigo.cs
+++ Assets/Scripts/Scripts Enemigos/Enemigo.cs
@@ -6,17 +6,25 @@ using UnityEngine.AI;
 public class Enemigo : MonoBehaviour
 {
     public NavMeshAgent enemigo;
-    public Transform jugador;
+    public GameObject jugador;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        jugador = GameObject.Find("Jugador");
     }
 
     // Update is called once per frame
     void Update()
     {
-        enemigo.SetDestination(jugador.position);
+        enemigo.SetDestination(jugador.transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Projectile"))
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
--- Assets/Scripts/Scripts Enemigos/SpawnEnemigos.cs
+++ Assets/Scripts/Scripts Enemigos/SpawnEnemigos.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class SpawnEnemigos : MonoBehaviour
 {
+    public Transform[] spawns;
+
+    public GameObject enemigoPrefab;
+
+    public bool seguir = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Spawns());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    public IEnumerator Spawns()
+    {
+        while (seguir)
+        {
+            yield return new WaitForSeconds(1f);
+
+            int index = Random.Range(0, spawns.Length);
+            Vector3 spawnPosition = spawns[index].position;
+
+            Instantiate(enemigoPrefab, spawnPosition, Quaternion.identity);
+        }
     }
 }
