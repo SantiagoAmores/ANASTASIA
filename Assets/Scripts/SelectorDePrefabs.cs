d0809b901248b6baea9b331bbdf269cc2936cfb8
Manuel13400 <57767398+Manuel13400@users.noreply.github.com>
2025-04-03 12:00:14 +0200

    menu desplegable en interfaz de unity
    Prueba para incluir un menu desplegable a la interfaz de Unity: La idea es que este script sirva para elegir que enemigo aparece en cada nivel. Ademas, se podria reutilizar mas adelante para crear mas desplegables

---
+++ Assets/Scripts/SelectorDePrefabs.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectorDePrefabs : MonoBehaviour
+{
+    // Objetos ocultos en la interfaz para que solo aparezcan el desplegable
+    [HideInInspector] public GameObject enemigo1;
+    [HideInInspector] public GameObject enemigo2;
+    [HideInInspector] public GameObject enemigoJefe;
+}
