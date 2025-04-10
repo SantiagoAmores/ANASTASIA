e58fbdf1e2c880c0d3303f71c8e24015993fe5b0
SantiagoAmores <santi251998@gmail.com>
2025-04-10 10:24:52 +0200

    prefab tostadora

--- Assets/Scripts/Scripts Enemigos/Enemigo.cs
+++ Assets/Scripts/Scripts Enemigos/Enemigo.cs
@@ -12,11 +12,24 @@ public class Enemigo : MonoBehaviour
 
     private CanvasManager canvasManager;
 
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         jugador = GameObject.FindGameObjectWithTag("Player");
         canvasManager = FindObjectOfType<CanvasManager>();
+
+        //animator = GetComponent<Animator>();  // Obtener el Animator del enemigo
+
+        //animator.Play("Walking"); //Animacion
+
+        //// Iniciar la animación de caminar
+        //if (animator != null)
+        //{
+        //    animator.SetBool("isWalking", true);  // Asegúrate de que 'isWalking' es un parámetro booleano en tu Animator
+        //}
+
     }
 
     // Update is called once per frame
