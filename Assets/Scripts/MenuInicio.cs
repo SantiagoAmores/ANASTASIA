1d548acaa9dcb1567db38ee976c4cf874d3ad314
Heidism15 <heidism1508@gmail.com>
2025-04-20 04:10:14 +0200

    Menu inicio renovado
    Escena 3d con cinemachine y menu integrado en ella :) 🍎

--- Assets/Scripts/MenuInicio.cs
+++ Assets/Scripts/MenuInicio.cs
@@ -1,21 +1,26 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 using UnityEngine.SceneManagement;
 
 public class MenuInicio : MonoBehaviour
 {
-    public GameObject opciones;
+    /*public GameObject opciones;
     public GameObject botonAtras;
     public GameObject botonSalirJuego;
-    public GameObject menuPrincipal;
+    public GameObject menuPrincipal;*/
+
+    public CinemachineVirtualCamera mainMenuCam;
+    public CinemachineVirtualCamera optionsCam;
 
     // Start is called before the first frame update
     void Start()
     {
-        menuPrincipal.SetActive(true);
+        /*menuPrincipal.SetActive(true);
         opciones.SetActive(false);
-        botonAtras.SetActive(false);
+        botonAtras.SetActive(false);*/
     }
 
     // Update is called once per frame
@@ -32,16 +37,23 @@ public class MenuInicio : MonoBehaviour
 
     public void MostrarOpciones()
     {
+        mainMenuCam.Priority = 0;
+        optionsCam.Priority = 10;
+
+        /*
         menuPrincipal.SetActive(false);
         opciones.SetActive(true);
-        botonAtras.SetActive(true);
+        botonAtras.SetActive(true);*/
     }
 
     public void VolverMenu()
     {
+        mainMenuCam.Priority = 10;
+        optionsCam.Priority = 0;
+        /*
         menuPrincipal.SetActive(true);
         botonAtras.SetActive(false);
-        opciones.SetActive(false);
+        opciones.SetActive(false);*/
     }
 
     public void SalirJuego()
