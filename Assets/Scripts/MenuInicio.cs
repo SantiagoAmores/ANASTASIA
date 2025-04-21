e15b7116e5125f884dbb6c8002b754bbc214040d
Manuel <manueldelarosa1340@gmail.com>
2025-04-21 03:20:53 +0200

    segurata para el final del museo

--- Assets/Scripts/MenuInicio.cs
+++ Assets/Scripts/MenuInicio.cs
@@ -2,7 +2,6 @@ using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Cinemachine;
 using UnityEngine.SceneManagement;
 
 public class MenuInicio : MonoBehaviour
