d0398ba639c671b6c6ac69077dda11df1c79077c
Santi <151826439+SantiagoAmores@users.noreply.github.com>
2025-04-24 01:10:19 +0200

    nivel 1 arreglada escena

---
+++ Assets/Scripts/Tostada.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tostada : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            
+           
+        }
+
+        else
+        {
+
+
+
+        }
+    }
+}
+
